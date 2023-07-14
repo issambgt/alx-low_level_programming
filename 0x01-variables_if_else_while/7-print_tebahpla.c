e03eb93fe2fab3c428610412fd2099e6c1e09970
issambgt <issambgt03@gmail.com>
2023-07-14 16:49:55 -0700

    a

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: print alphabets in reverse
+ *
+ * Return: 0 (success)
+ */
+
+
+int main(void)
+{
+int n = 122;
+
+while (n >= 97)
+{
+
+putchar(n);
+n--;
+}
+putchar('\n');
+
+return (0);
+}
