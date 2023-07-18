20c2e1523c353ffc2e9729bed4e53842e6003519
issambgt <issambgt03@gmail.com>
2023-07-18 08:56:41 -0700

    a

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -4,7 +4,7 @@
  * print_sign - Determines if the input number
  *greater, iqual or less than zero.
  *
- *@c: The input number as an integer.
+ *@n: The input number as an integer.
  *
  * Return: 1 is greater than zero. 0 is zero.
  * -1 is less than zero.
